f2cc861406b36939761393ae9430d28667a738d3
AjeetVIT <ajeetvit@gmail.com>
2022-02-21 12:30:53 +0530

    first commit

---
+++ src/main/java/in/nareshit/ajeet/hc/entity/Specialization.java
@@ -0,0 +1,24 @@
+package in.nareshit.ajeet.hc.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="specializaton_tab")
+public class Specialization {
+@Id
+@GeneratedValue(strategy=GenerationType.AUTO)
+@Column(name="spec_id_col")
+private Long id;
+@Column(name="spec_code_col")
+private String specCode;
+@Column(name="spec_name_col")
+private String specName;
+@Column(name="spec_note_col")
+private String specNote;
+
+}
---
+++ src/main/java/in/nareshit/ajeet/hc/repo/SpecializtionRepository.java
@@ -0,0 +1,9 @@
+package in.nareshit.ajeet.hc.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import in.nareshit.ajeet.hc.entity.Specialization;
+
+public interface SpecializtionRepository extends JpaRepository<Specialization,Long> {
+
+}
